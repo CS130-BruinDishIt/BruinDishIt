cab07dc7a61f09214fad2a54dc97499adb58f128
Shane Song <ssong17@g.ucla.edu>
2026-05-22 11:49:53 -0700

    requireAuth.js is a function that is called when users try to access features that require logging in. Also installed a .env file to store information involving json web tokens

--- server/authentication/routes.js
+++ server/authentication/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { loginUser } from "./login.js";
+import { signToken } from "./requireAuth.js";
 
 const router = Router();
 
@@ -12,7 +13,10 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ error: result.message });
     }
 
-    return res.status(200).json({ user: result.user });
+    return res.status(200).json({
+      user: result.user,
+      token: signToken(result.user._id),
+    });
   } catch (err) {
     console.error("Login error:", err);
     return res.status(500).json({ error: "Login failed." });
